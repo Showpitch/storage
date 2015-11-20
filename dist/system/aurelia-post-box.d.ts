2ca02f11df08dd9db14d1ee1c37b370980714567
Eric Johnson <ejohnson@showpitch.com>
2015-11-20 10:22:34 -0600

    Linted and prepared release

---
+++ dist/system/aurelia-post-box.d.ts
@@ -0,0 +1,13 @@
+declare module 'aurelia-post-box' {
+  
+  /**
+   * Created by ericjohnson on 10/6/15.
+   */
+  export class Storage {
+    constructor();
+    getPersistedTopics(): any;
+    saveTopic(topic: any, value: any, isLocal?: any): any;
+    retrieveTopic(topic: any): any;
+    deleteTopic(topic: any): any;
+  }
+}
---
+++ dist/system/aurelia-post-box.js
@@ -0,0 +1,72 @@
+System.register([], function (_export) {
+    'use strict';
+
+    var Storage;
+
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+    return {
+        setters: [],
+        execute: function () {
+            Storage = (function () {
+                function Storage() {
+                    _classCallCheck(this, Storage);
+
+                    this.persistedTopics = {};
+                    this.storage = window.localStorage;
+                    this.session = window.sessionStorage;
+
+                    this.getPersistedTopics();
+                }
+
+                Storage.prototype.getPersistedTopics = function getPersistedTopics() {
+                    var x = Object.keys(this.storage);
+
+                    for (var i = 0; i < x.length; i++) {
+                        this.persistedTopics[x[i]] = true;
+                    }
+                };
+
+                Storage.prototype.saveTopic = function saveTopic(topic, value) {
+                    var isLocal = arguments.length <= 2 || arguments[2] === undefined ? false : arguments[2];
+
+                    if (topic === undefined || value === undefined) {
+                        return;
+                    }
+
+                    if (typeof value == 'object') {
+                        value = JSON.stringify(value);
+                    }
+
+                    if (isLocal) {
+                        this.persistedTopics[topic] = true;
+                        this.storage.setItem(topic, value);
+                    } else {
+                        this.session.setItem(topic, value);
+                    }
+                };
+
+                Storage.prototype.retrieveTopic = function retrieveTopic(topic) {
+                    if (this.persistedTopics[topic]) {
+                        return JSON.parse(this.storage.getItem(topic));
+                    } else {
+                        return JSON.parse(this.session.getItem(topic));
+                    }
+                };
+
+                Storage.prototype.deleteTopic = function deleteTopic(topic) {
+                    if (this.persistedTopics[topic]) {
+                        delete this.persistedTopics[topic];
+                        this.storage.removeItem(topic);
+                    } else {
+                        this.session.removeItem(topic);
+                    }
+                };
+
+                return Storage;
+            })();
+
+            _export('Storage', Storage);
+        }
+    };
+});
