fd14d28c119fd7a4ac319bfe6f6ea3412fecc406
Eric Johnson <ejohnson@showpitch.com>
2016-02-01 12:15:19 -0700

    Updating output files

---
+++ dist/system/storage.d.ts
@@ -0,0 +1,18 @@
+declare module 'storage' {
+  
+  /**
+   * Created by ericjohnson on 10/6/15.
+   */
+  import moment from 'moment';
+  export class Storage {
+    constructor();
+    loadIndex(): any;
+    store(key: any, value: any, expiration?: any, session?: any): any;
+    retrieve(key: any): any;
+    remove(key: any): any;
+    saveTopic(topic: any, value: any, isLocal?: any): any;
+    retrieveTopic(topic: any): any;
+    deleteTopic(topic: any): any;
+    isJsonString(str: any): any;
+  }
+}
